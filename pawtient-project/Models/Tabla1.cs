c7203412a3ee22b42c7d222c5ba83e7c8b647a1a
brianna-salinas <zevensos@gmail.com>
2026-06-12 17:50:43 -0500

    Starter project

---
+++ pawtient-project/Models/Tabla1.cs
@@ -0,0 +1,9 @@
+﻿namespace pawtient_project.Models;
+
+public class Tabla1
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
